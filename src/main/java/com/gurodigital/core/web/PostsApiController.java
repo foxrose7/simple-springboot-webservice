9bac38be6877a546d322e167da5c59ed3cead217
foxrose7 <foxrose@kead.or.kr>
2023-01-27 00:14:54 +0900

    PostController 와 Post 관련 DTO 생성

---
+++ src/main/java/com/gurodigital/core/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.gurodigital.core.web;
+
+import com.gurodigital.core.service.PostsService;
+import com.gurodigital.core.web.dto.PostsResponseDto;
+import com.gurodigital.core.web.dto.PostsSaveRequestDto;
+import com.gurodigital.core.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
